cf941af807900200e17dad997b6020a2efa8628e
chimcen <22658487+chimcen@users.noreply.github.com>
2018-10-11 12:00:18 +0200

    added Preassure at Sealevel

--- nodemcu/lua/bme.lua
+++ nodemcu/lua/bme.lua
@@ -2,21 +2,21 @@ local module = {}
 
 function module.read()
 
-  local alt = 320 -- altitude of the measurement place
+  local alt = 525 -- altitude of the measurement place
   i2c.setup(0,config.bme280sda, config.bme280scl,i2c.SLOW)
   local device = bme280.setup()
   local status, temp, humi, baro, dew
 
   if device == 2 then
     status = 0
-    local T,P,H,QNH = bme280.read()
+    local T,P,H,QNH = bme280.read(alt)
     while T == nil do
       tmr.delay(100)
       T,P,H,QNH = bme280.read()
     end
-
     baro = P / 1000
-    temp = T / 100
+    baros = QNH / 1000 
+    temp = T / 101
     humi = H / 1000
 
     local D = bme280.dewpoint(H, T)
@@ -33,7 +33,7 @@ function module.read()
 
   end
 
-  return status, temp, humi, baro, dew
+  return status, temp, humi, baro, baros, dew
 
 end
 
